d2d9f3ec62f80daa7edf70460140e97042032fc6
Aqeel <aqeelten@gmail.com>
2023-08-19 18:47:42 +0530

    f: update

--- frontend/skillsift/src/app/start/page.tsx
+++ frontend/skillsift/src/app/start/page.tsx
@@ -1,7 +1,25 @@
-export default function Page(){
+export default function Page() {
     return <div className="flex w-screen h-screen flex-col items-center text-black p-5">
-        <input type="file"/>
+        <input type="file" />
         <div>OR</div>
-        
+        <div className="form-control w-full max-w-[50vh]">
+
+            <label className="label mt-4">
+                <span className="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Email" className="input input-bordered w-full" />
+            <label className="label mt-4">
+                <span className="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Email" className="input input-bordered w-full" />
+            <label className="label mt-4">
+                <span className="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Email" className="input input-bordered w-full" />
+            <label className="label mt-4">
+                <span className="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Email" className="input input-bordered w-full" />
+        </div>
     </div>
 }
