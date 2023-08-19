563c1baee7faf920215d18853e8202d4310c6e2d
Abdul Majid <majid616365@gmail.com>
2023-08-19 14:31:57 +0530

    added daisy ui

--- frontend/skillsift/src/app/page.tsx
+++ frontend/skillsift/src/app/page.tsx
@@ -2,8 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Skillsift</h1>
+    <div className="">
+      <div className="navbar">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">SkillSift</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
