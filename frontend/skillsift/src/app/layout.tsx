11757e47ed06874cc9f073bafab98ce28d0ad8b1
Aqeel <aqeelten@gmail.com>
2023-08-19 18:33:35 +0530

    f: update

--- frontend/skillsift/src/app/layout.tsx
+++ frontend/skillsift/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  weight: [
+    "200", "300", "400", "500", "600", "700", "800", "900"
+  ], subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: 'SkillSift',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${poppins.className}` + " overflow-hidden"}>{children}</body>
     </html>
   )
 }
--- frontend/skillsift/src/app/page.tsx
+++ frontend/skillsift/src/app/page.tsx
@@ -1,9 +1,50 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { FiX } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <div className="w-screen">
-      
+    <div className="flex w-screen h-screen flex-col text-black p-5">
+      <nav className="flex justify-between">
+        <p className="text-2xl font-bold">SkillSift</p>
+        <div className="flex">
+          <button className="btn mr-2">For Organizations</button>
+          <label htmlFor="login_modal" className="btn btn-primary" >Login</label>
+        </div>
+      </nav>
+      <main className="w-full h-full flex justify-center items-center flex-col">
+        <p className="text-4xl">Lorem ipsum dolor sit amet.</p>
+        <p className="text-lg mt-4">Lorem ipsum dolor sit amet lorem ipsum dolor sit amet. This is an example of description text.</p>
+        <Link href="/start"><button className="btn btn-primary mt-10">Get Started</button></Link>
+      </main>
+      {/* Login */}
+      <input type="checkbox" id="login_modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <div className="flex justify-between items-center">
+            <h3 className="font-bold text-lg">Login</h3>
+            <label htmlFor="login_modal" className="btn btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+            </label>
+          </div>
+          <div className="form-control w-full">
+            <label className="label mt-4">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Email" className="input input-bordered w-full" />
+            <label className="label mt-4">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="Password" className="input input-bordered w-full" />
+          </div>
+          <div className="modal-action">
+            <button className="btn btn-primary w-full">Login</button>
+          </div>
+          <div className="flex justify-center">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
