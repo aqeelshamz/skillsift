fba46f0094a74c5100d70ff146926af5d43e6094
Aqeel <aqeelten@gmail.com>
2023-08-19 16:34:33 +0530

    f: update

--- frontend/skillsift/src/app/page.tsx
+++ frontend/skillsift/src/app/page.tsx
@@ -2,32 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="navbar">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">SkillSift</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Link</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div className="w-screen">
+      
     </div>
   );
 }
