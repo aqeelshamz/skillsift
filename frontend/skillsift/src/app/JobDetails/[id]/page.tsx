65cc0ae1b4280d964cbb26ca6f3ad924c925aa47
anuragrajanp <anuragrajanp@gmail.com>
2023-08-20 09:49:32 +0530

    jobdetails

--- frontend/skillsift/src/app/JobDetails/[id]/page.tsx
+++ frontend/skillsift/src/app/JobDetails/[id]/page.tsx
@@ -12,28 +12,41 @@ export default function Page() {
 		"Cloud Computing",
 		"Cybersecurity",
 		"Networking",
+		"DevOps",
+		"Artificial Intelligence and Machine Learning",
+		"Agile Methodology",
+		"Problem Solving and Debugging",
 	];
 
 	return (
-		<div className="bg-white w-full h-full overflow-auto m-10 ">
-			<div className="md:text-5xl text-3xl font-extrabold mt-4 p-3 md:p-5 ">
+		<div className="bg-white w-full h-full overflow-auto ">
+			<div className="md:text-5xl text-3xl font-extrabold mt-4 p-3 md:p-5">
 				Software Engineer (Remote)
 			</div>
 			<div className="md:text-3xl text-xl font-semibold opacity-60 p-3 md:px-5 ">
 				Google
 			</div>
-			<div className="">
+			<div className="md:flex">
 				<div className="">
-					<div className="md:text-2xl text-lg max-w-[1100px] text-justify opacity-90 p-3 md:px-5">
-						As a Software Engineer at Google, you will be part of a
-						diverse and dynamic team that creates groundbreaking
-						solutions to complex challenges. Your role will involve
-						developing and optimizing software that powers some of
-						the most widely used products and services globally.
+					<div className="md:text-2xl text-lg max-w-[1100px] text-justify  font-semibold opacity-90 p-3 md:px-5 ">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Quisque gravida, mauris eget varius malesuada, nunc
+						risus laoreet odio, vel lacinia nisi orci id justo.
+						Pellentesque id feugiat felis. Maecenas ac urna eu velit
+						facilisis dignissim. Praesent nec arcu sit amet sem
+						euismod ullamcorper. Proin facilisis justo non risus
+						tincidunt, a venenatis purus congue. Vestibulum ante
+						ipsum primis in faucibus orci luctus et ultrices posuere
+						cubilia Curae; Quisque vitae mi ac nulla fermentum
+						tincidunt. Vivamus lacinia, quam et vulputate porttitor,
+						tortor velit gravida est, vel fermentum nulla arcu non
+						augue. Donec sed eros et odio facilisis dignissim.
+						Suspendisse potenti.
 					</div>
 					<div className="hidden md:block">
-						<div className=" md:text-3xl text-xl  font-semibold p-3 md:px-5 md:flex sm:items-center  mb-5">
+						<div className=" md:text-3xl text-xl  font-semibold p-3 md:px-5 md:flex sm:items-center mt-10 mb-20">
 							<div className="flex">
+								{" "}
 								<BsCashStack
 									className="opacity-60 mr-1"
 									size={30}
@@ -45,19 +58,19 @@ export default function Page() {
 								type="button"
 								className="text-white sm:ml-7 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-lg px-5 py-5 mr-2 mb-2  "
 							>
-								Apply Now
+								Apply Now !
 							</button>
 						</div>
 					</div>
 				</div>
 				<div className="">
-					<div className="md:text-3xl text-xl font-semibold opacity-60 p-1 flex">
-						<SiMusicbrainz className="opacity-90 " size={30} />{" "}
+					<div className="md:text-3xl text-xl font-semibold opacity-60 p-3 md:px-5 flex">
+						<SiMusicbrainz className="opacity-90 mr-1" size={30} />{" "}
 						Skill Required{" "}
 					</div>
 					{skill.map((item: string, index: number) => (
 						<li
-							className="md:text-xl text-sm font-semibold opacity-90 p-3 md:px-5 flex"
+							className="md:text-3xl text-xl font-semibold opacity-90 p-3 md:px-5 flex"
 							key={index}
 						>
 							{item}
